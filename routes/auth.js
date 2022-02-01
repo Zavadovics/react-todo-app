2f76df6fe91cdf17191f2da9687a9935b63e1708
Zavadovics <zavadovics@gmail.com>
2022-02-01 10:55:50 +0100

    completed API endpoints

--- routes/auth.js
+++ routes/auth.js
@@ -46,6 +46,18 @@ router.post('/register', async (req, res) => {
     // save the user to db
     const savedUser = await newUser.save();
 
+    const payload = { userId: savedUser._id };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: '7d',
+    });
+
+    res.cookie('access-token', token, {
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+    });
+
     // remove password so it's not returned
     const userToReturn = { ...savedUser._doc };
     delete userToReturn.password;
@@ -104,9 +116,8 @@ router.post('/login', async (req, res) => {
       token: token,
       user: userToReturn,
     });
-  } catch (err) {
-    console.log(err);
-
+  } catch (error) {
+    console.log(error);
     return res.status(500).send(err.message);
   }
 });
@@ -117,6 +128,21 @@ router.post('/login', async (req, res) => {
 router.get('/current', requiresAuth, (req, res) => {
   if (!req.user) {
     return res.status(401).send('Unauthorized');
+  }
+
+  return res.json(req.user);
+});
+
+// @route   PUT /api/auth/logout
+// @desc    Logout the currently authed user
+// @access  Public
+router.put('/logout', requiresAuth, async (req, res) => {
+  try {
+    res.clearCookie('access-token');
+    return res.json({ message: 'User has been logged out' });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(err.message);
   }
 
   return res.json(req.user);
